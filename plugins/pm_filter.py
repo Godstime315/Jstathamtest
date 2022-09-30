e5b5053d8de785f89690431239a2748fcdc18d87
Godstime315 <97334195+Godstime315@users.noreply.github.com>
2022-09-30 15:53:16 +0100

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -125,7 +125,7 @@ async def next_page(bot, query):
         btn.append(
             [
                 InlineKeyboardButton("🔙 ᴩᴀɢᴇ", callback_data=f"next_{req}_{key}_{off_set}"),
-                InlineKeyboardButton(f"🗓 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
+                InlineKeyboardButton(f"🗓 {math.ceil(int(offset) / 5) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
                 InlineKeyboardButton("ɴᴇxᴛ ⏭️", callback_data=f"next_{req}_{key}_{n_offset}")
             ],
         )
